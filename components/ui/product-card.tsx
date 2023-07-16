cf0ef17f960f53f0ba4eed3b8adafcbca9d2b6b2
OmarMWarraich <earthling986@gmail.com>
2023-07-16 16:12:38 +0500

    feat: Product Preview Modal Components

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Image from 'next/image';
+import { MouseEventHandler } from 'react';
 import { Expand, ShoppingCart } from 'lucide-react';
 
 import { Product } from '@/types';
 import IconButton from '@/components/ui/icon-button';
 import Currency from '@/components/ui/currency';
 import { useRouter } from 'next/navigation';
+import usePreviewModal from '@/hooks/use-preview-modal';
 
 interface ProductCardProps {
     data: Product;
@@ -15,11 +17,18 @@ interface ProductCardProps {
 const ProductCard:React.FC<ProductCardProps> = ({
     data
 }) => {
+    const previewModal = usePreviewModal();
     const router = useRouter();
 
     const handleClick = () => {
         router.push(`/product/${data?.id}`);
     }
+
+    const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+    
+        previewModal.onOpen(data);
+      };
  
     return (
 
@@ -34,7 +43,7 @@ const ProductCard:React.FC<ProductCardProps> = ({
             <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                 <div className="flex gap-x-6 justify-center">
                     <IconButton
-                        onClick={() => console.log("Add to cart")}
+                        onClick={onPreview}
                         icon={<Expand size={20} className="text-gray-600" />} />
                     <IconButton
                         onClick={() => console.log("Add to cart")}
